95ce7f957c0d0be3ba7b92522ce1d0d5f35a620b
Niels Opdam <Niels Opdam>
2016-04-13 17:06:29 +0200

    Basic TCP server test

--- apps/master/src/master_app.erl
+++ apps/master/src/master_app.erl
@@ -8,7 +8,7 @@
 -behaviour(application).
 
 %% Application callbacks
--export([start/2, stop/1, test_test/0]).
+-export([start/2, stop/1]).
 -include_lib("eunit/include/eunit.hrl").
 
 
@@ -28,6 +28,47 @@ stop(_State) ->
 %%====================================================================
 
 test_test() ->
-  Var = hrp_pb:decode_message(iolist_to_binary(hrp_pb:encode({message, "abc123", "Dit is de tekst", "Raoul"}))),
-  ?debugFmt("Function fun1 starting...", [Var]),
+  start_server(31337),
+  timer:sleep(1000000),
+  Binary = hrp_pb:encode(
+    [
+      {message, "abc123", "Dit is de tekst", "Raoul"},
+      {message, "def", "Dit is de tekst!!!", "Niels"}
+    ]),
+  ?debugFmt("~n~p~n", [iolist_to_binary(Binary)]),
   ?assert(true).
+
+start_server(Port) ->
+  Pid = spawn_link(fun() ->
+    {ok, Listen} = gen_tcp:listen(Port, [binary, {active, true}]),
+    spawn(fun() -> acceptor(Listen) end),
+    timer:sleep(infinity)
+                   end),
+  {ok, Pid}.
+
+acceptor(ListenSocket) ->
+  {ok, Socket} = gen_tcp:accept(ListenSocket),
+  spawn(fun() -> acceptor(ListenSocket) end),
+  handle(Socket).
+
+%% Echoing back whatever was obtained
+handle(Socket) ->
+  inet:setopts(Socket, [{active, once}]),
+  receive
+    {tcp, Socket, <<"quit", _/binary>>} ->
+      gen_tcp:close(Socket);
+    {tcp, Socket, Msg} ->
+      Response = handle_message(Msg),
+      gen_tcp:send(Socket, Response),
+      handle(Socket)
+  end.
+
+handle_message(Msg) ->
+  Data = hrp_pb:delimited_decode_message(list_to_binary(Msg)),
+  MessageType = element(1, hd(Data)),
+  case MessageType of
+    message ->
+      hrp_pb:encode({message, "id", "text", "username"});
+    _ ->
+      ok
+  end.
