3ee00b9499329843e037501f6b7171850b5483ed
RaoulMeyer <raouljmeyer@gmail.com>
2016-04-15 18:27:56 +0200

    ASDKN-200 Added get graph update function

--- apps/master/src/node_graph_manager.erl
+++ apps/master/src/node_graph_manager.erl
@@ -3,3 +3,33 @@
 %% @end
 %%%-------------------------------------------------------------------
 -module(node_graph_manager).
+
+-export([get_graph_updates/1]).
+
+get_graph_updates(Version) ->
+    get_graph_updates_for_versions(
+        get_version_numbers_since(
+            max(Version, get_min_version())
+        )
+    ).
+
+get_version_numbers_since(Version) ->
+    lists:seq(Version + 1, get_max_version()).
+
+get_min_version() ->
+    %% Redis get
+    10.
+
+get_max_version() ->
+    %% Redis get
+    12.
+
+get_graph_updates_for_versions(Versions) ->
+    lists:map(
+        fun(Version) -> get_graph_updates_for_version(Version) end,
+        Versions
+    ).
+
+get_graph_updates_for_version(Version) ->
+    %% Redis get
+    {Version, false, [], [], []}.
--- apps/master/test/graph_SUITE.erl
+++ apps/master/test/graph_SUITE.erl
@@ -20,20 +20,13 @@ all() -> [node_graph_manager_test].
 
 %%initial for datastructure graph, not right yet
 init_per_testcase(node_graph_manager_test, Config) ->
-  CurrentVersion = ets:new(graph, [ordered_set, public]),
-  AddedNodes = ets:new(addednodes, [ordered_set, public]),
-  Node1Edges = ets:new(node1edges, [ordered_set, public]),
-  ets:insert(Node1Edges, {"2", 2.0}),
-  ets:insert(Node1Edges, {"3", 3.0}),
-  ets:insert(Node1Edges, {"4", 4.0}),
-  ets:insert(AddedNodes, {"1", "135.346.334.3", "8643", "sfhjrerhre", Node1Edges}),
-  ets:insert(CurrentVersion, {1, true, AddedNodes, [],[]}),
-  [{table,CurrentVersion} | Config].
+    Config.
 
 end_per_testcase(node_graph_manager_test, Config) ->
-  ets:delete(?config(table, Config)).
+    Config.
 
 %%test will fail, no logical test
 node_graph_manager_test(Config) ->
-   CurrentVersion = ?config(table, Config),
-    [{david, 12}] = ets:lookup(CurrentVersion, david).
+    [{12, false, [], [], []}] = node_graph_manager:get_graph_updates(11),
+    [{11, false, [], [], []}, {12, false, [], [], []}] = node_graph_manager:get_graph_updates(10),
+    [{11, false, [], [], []}, {12, false, [], [], []}] = node_graph_manager:get_graph_updates(-100).
