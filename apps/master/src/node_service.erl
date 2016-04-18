871bbd93fa1e28be6d35ff5d0dde1e61700d39df
Rik Harink <rik@blisss.nl>
2016-04-18 11:23:22 +0200

    Beatuified the tests and wrote some more tests for node unregistering

--- apps/master/src/node_service.erl
+++ apps/master/src/node_service.erl
@@ -8,12 +8,12 @@
 
 %% @doc Register your node in the graph
 %% @end
-node_register(IPaddress, Port, PublicKey) when is_integer(Port) -> 
-    ok = node_graph_manager:add_node(IPaddress, Port, PublicKey),
+node_register(IPaddress, Port, PublicKey) when is_list(IPaddress), is_integer(Port), is_list(PublicKey) -> 
+    node_graph_manager:add_node(IPaddress, Port, PublicKey),
     heartbeat_monitor:add_node(IPaddress, Port, PublicKey).
 
 %% @doc Unregister your node in the graph
 %% @end
-node_unregister(IPaddress, Port, PublicKey) when is_integer(Port) ->
-    ok = node_graph_manager:remove_node(IPaddress, Port, PublicKey),
+node_unregister(IPaddress, Port, PublicKey) when is_list(IPaddress), is_integer(Port), is_list(PublicKey) ->
+    node_graph_manager:remove_node(IPaddress, Port, PublicKey),
     heartbeat_monitor:remove_node(IPaddress, Port, PublicKey).
--- apps/master/test/node_service_SUITE.erl
+++ apps/master/test/node_service_SUITE.erl
@@ -2,16 +2,17 @@
 -include_lib("common_test/include/ct.hrl").
 
 -export([all/0, init_per_testcase/2, end_per_testcase/2]).
--export([node_register_test_valid_node/1, node_register_test_invalid_node/1, node_unregister_test/1]).
+-export([node_register_test_valid_node/1, node_register_test_invalid_node/1, node_unregister_test_valid_node/1, node_unregister_test_invalid_node/1]).
 
-all() -> [node_register_test_valid_node, node_unregister_test, node_register_test_invalid_node].
+all() -> [node_register_test_valid_node, node_unregister_test_valid_node, 
+          node_register_test_invalid_node, node_unregister_test_invalid_node].
 
 init_per_testcase(_, Config) ->
     IPaddress = "192.168.4.4",
     Port = 1337,
     PublicKey = "MyPublicKey",
     ValidNode = {IPaddress, Port, PublicKey},
-    InvalidNode = {IPaddress, "Harry", PublicKey},
+    InvalidNode = {42, "Harry", 42},
     [{validnode,ValidNode},  {invalidnode, InvalidNode}] ++ Config.
 
 end_per_testcase(_, Config) ->
@@ -20,19 +21,37 @@ end_per_testcase(_, Config) ->
 node_register_test_valid_node(Config) -> 
     {IPaddress, Port, PublicKey} = ?config(validnode, Config),
     meck:new(node_graph_manager, [non_strict, passthrough]),
-    meck:expect(node_graph_manager, add_node, fun(IPaddress, Port, PublicKey) -> ok end),
+    meck:expect(node_graph_manager, add_node, fun(_,_,_) -> ok end),
     meck:new(heartbeat_monitor, [non_strict, passthrough]),
-    meck:expect(heartbeat_monitor, add_node, fun(IPaddress, Port, PublicKey) -> ok end),
+    meck:expect(heartbeat_monitor, add_node, fun(_,_,_) -> ok end),
     ok = node_service:node_register(IPaddress, Port, PublicKey).
 
 node_register_test_invalid_node(Config) ->
-    {IPaddress, Port, PublicKey} = ?config(invalidnode, Config),
-    meck:new(node_graph_manager, [non_strict, passthrough]),
-    meck:expect(node_graph_manager, add_node, fun(IPaddress, Port, PublicKey) -> ok end),
-    meck:new(heartbeat_monitor, [non_strict, passthrough]),
-    meck:expect(heartbeat_monitor, add_node, fun(IPaddress, Port, PublicKey) -> ok end),
-    test_helpers:assert_fail(fun node_service:node_register/3, [IPaddress, Port,PublicKey], error, function_clause, failed_to_catch_invalid_argument).
-
-node_unregister_test(Config) ->
     {IPaddress, Port, PublicKey} = ?config(validnode, Config),
+    {InvIPaddress, InvPort, InvPublicKey} = ?config(invalidnode, Config),
+    meck:new(node_graph_manager, [non_strict, passthrough]),
+    meck:expect(node_graph_manager, add_node, fun(_,_,_) -> ok end),
+    meck:new(heartbeat_monitor, [non_strict, passthrough]),
+    meck:expect(heartbeat_monitor, add_node, fun(_,_,_) -> ok end),
+    test_helpers:assert_fail(fun node_service:node_register/3, [InvIPaddress, Port, PublicKey], error, function_clause, failed_to_catch_invalid_argument),
+    test_helpers:assert_fail(fun node_service:node_register/3, [IPaddress, InvPort,PublicKey], error, function_clause, failed_to_catch_invalid_argument),
+    test_helpers:assert_fail(fun node_service:node_register/3, [IPaddress, Port, InvPublicKey], error, function_clause, failed_to_catch_invalid_argument).
+
+node_unregister_test_valid_node(Config) ->
+    {IPaddress, Port, PublicKey} = ?config(validnode, Config),
+    meck:new(node_graph_manager, [non_strict, passthrough]),
+    meck:expect(node_graph_manager, remove_node, fun(_,_,_) -> ok end),
+    meck:new(heartbeat_monitor, [non_strict, passthrough]),
+    meck:expect(heartbeat_monitor, remove_node, fun(_,_,_) -> ok end),
     node_service:node_unregister(IPaddress, Port, PublicKey).
+
+node_unregister_test_invalid_node(Config) ->
+    {IPaddress, Port, PublicKey} = ?config(validnode, Config),
+    {InvIPaddress, InvPort, InvPublicKey} = ?config(invalidnode, Config),
+    meck:new(node_graph_manager, [non_strict, passthrough]),
+    meck:expect(node_graph_manager, remove_node, fun(_,_,_) -> ok end),
+    meck:new(heartbeat_monitor, [non_strict, passthrough]),
+    meck:expect(heartbeat_monitor, remove_node, fun(_,_,_) -> ok end),
+    test_helpers:assert_fail(fun node_service:node_unregister/3, [InvIPaddress, Port,PublicKey], error, function_clause, failed_to_catch_invalid_argument),
+    test_helpers:assert_fail(fun node_service:node_unregister/3, [IPaddress, InvPort, PublicKey], error, function_clause, failed_to_catch_invalid_argument),
+    test_helpers:assert_fail(fun node_service:node_unregister/3, [IPaddress, Port, InvPublicKey], error, function_clause, failed_to_catch_invalid_argument).
