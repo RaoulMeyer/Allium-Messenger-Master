a4a7f56e705f4261d24f0d7a6ae352ea8da28fb5
RaoulMeyer <raouljmeyer@gmail.com>
2016-04-19 08:59:24 +0200

    Fixed ASDKN-220 ASDKN-221 ASDKN-223 ASDKN-224 ASDKN-225 ASDKN-226 Halfway done with ASDKN-222

--- apps/master/src/node_graph_manager.erl
+++ apps/master/src/node_graph_manager.erl
@@ -4,25 +4,30 @@
 %%%-------------------------------------------------------------------
 -module(node_graph_manager).
 
--export([get_graph_updates/1]).
+-export([get_graph_updates/1, rebuild_graph/0, rebuild_graph_at_interval/1, fill_data/0]).
 
-get_graph_updates(Version) ->
-    get_graph_updates_for_versions(
-        get_version_numbers_since(
-            max(Version, get_min_version())
-        )
-    ).
+get_graph_updates(Version) when is_integer(Version) ->
+    RequestedVersion = max(Version, get_min_version()),
+    GetMaxVersion = get_max_version(),
+    case RequestedVersion of
+        GetMaxVersion ->
+            [];
+        _ ->
+            get_graph_updates_for_versions(
+                get_version_numbers_since(
+                    RequestedVersion
+                )
+            )
+    end.
 
 get_version_numbers_since(Version) ->
     lists:seq(Version + 1, get_max_version()).
 
 get_min_version() ->
-    %% Redis get
-    10.
+    binary_to_integer(redis:get("min_version")).
 
 get_max_version() ->
-    %% Redis get
-    12.
+    binary_to_integer(redis:get("max_version")).
 
 get_graph_updates_for_versions(Versions) ->
     lists:map(
@@ -31,5 +36,116 @@ get_graph_updates_for_versions(Versions) ->
     ).
 
 get_graph_updates_for_version(Version) ->
-    %% Redis get
-    {Version, false, [], [], []}.
+    redis:get("version_" ++ integer_to_list(Version)).
+
+
+
+
+
+
+rebuild_graph() ->
+    NewMinVersion = get_new_min_version(),
+    Graph = build_graph(NewMinVersion),
+    save_graph(Graph, NewMinVersion),
+    remove_old_versions(NewMinVersion),
+    update_min_version(NewMinVersion).
+
+build_graph(NewMinVersion) ->
+    GraphUpdates = lists:takewhile(
+        fun({graphupdate, VersionNumber, _, _, _, _}) -> VersionNumber =< NewMinVersion end,
+        protobuf_list_to_tuple_list(get_graph_updates(NewMinVersion))
+    ),
+    io:format("UPDATES: ~p~n", [GraphUpdates]),
+    NewGraph = lists:foldl(
+        fun(Update, Graph) -> merge_update_with_graph(Update, Graph) end,
+        get_current_full_graph(),
+        GraphUpdates
+    ),
+    io:format("NEW GRAPH:~p~n", [NewGraph]),
+    {graphupdate, _, FullGraph, Added, Edited, Deleted} = NewGraph,
+    {graphupdate, NewMinVersion, FullGraph, Added, Edited, Deleted}.
+
+get_current_full_graph() ->
+    protobufs_to_tuple(
+        get_graph_updates_for_version(
+          get_min_version()
+        )
+    ).
+
+merge_update_with_graph(Update, Graph) ->
+    {_, _, ResultingAdditions, _,  _} = Graph,
+    {_, _, Additions, _, Deletes} = Update,
+    NewAdditions = ResultingAdditions ++ Additions,
+    lists:foldl(
+        fun({DeletedNodeId, _, _, _, _}, TotalAdditions) ->
+            lists:filter(
+                fun({NodeId, _, _, _, _}) -> NodeId =/= DeletedNodeId end,
+                TotalAdditions
+            )
+        end,
+        NewAdditions,
+        Deletes
+    ).
+
+update_min_version(NewMinVersion) ->
+    ok. %redis:set("min_version", NewMinVersion).
+
+get_new_min_version() ->
+    round((get_min_version() + get_max_version()) / 2),
+    12.
+
+save_graph(Graph, NewMinVersion) ->
+    redis:set("version_" ++ integer_to_list(NewMinVersion), Graph).
+
+remove_old_versions(NewMinVersion) ->
+    lists:map(
+        fun(Version) -> redis:remove("version_" ++ integer_to_list(Version)) end,
+        lists:seq(get_min_version(), NewMinVersion - 1)
+    ).
+
+protobuf_list_to_tuple_list(List) ->
+    lists:map(
+        fun(Item) -> protobufs_to_tuple(Item) end,
+        List
+    ).
+
+protobufs_to_tuple(Data) ->
+    io:format("~p~n", [Data]),
+    hrp_pb:decode_graphupdate(Data).
+
+
+
+
+%% Periodically rebuild the graph.
+rebuild_graph_at_interval(Interval) ->
+    timer:sleep(Interval),
+    rebuild_graph(),
+    rebuild_graph_at_interval(Interval).
+
+fill_data() ->
+    redis:set("version_10", hrp_pb:encode(
+        {graphupdate, 12345, false,
+            [{node, "2", "192.168.0.1", 80, "abcdef123456", [{edge, "1", 5.0}]}],
+            [],
+            []
+        }
+    )),
+    redis:set("version_11", hrp_pb:encode(
+        {graphupdate, 12345, false,
+            [{node, "5", "192.168.0.3", 80, "abc123", []}],
+            [],
+            []
+        }
+    )),
+    redis:set("version_12", hrp_pb:encode(
+        {graphupdate, 12345, false,
+            [{node, "3", "192.168.0.5", 80, "abcde,fbasfvgassf123456", [{edge, "1", 5.0}]}],
+            [],
+            []
+        }
+    )).
+
+
+
+
+
--- apps/master/test/graph_SUITE.erl
+++ apps/master/test/graph_SUITE.erl
@@ -14,19 +14,23 @@
 
 %% API
 -export([all/0, init_per_testcase/2, end_per_testcase/2]).
--export([node_graph_manager_test/1]).
+-export([node_graph_manager_test/1, rebuild_graph_test/1]).
 
-all() -> [node_graph_manager_test].
+all() -> [node_graph_manager_test, rebuild_graph_test].
 
 %%initial for datastructure graph, not right yet
-init_per_testcase(node_graph_manager_test, Config) ->
+init_per_testcase(_, Config) ->
     Config.
 
-end_per_testcase(node_graph_manager_test, Config) ->
+end_per_testcase(_, Config) ->
     Config.
 
 %%test will fail, no logical test
 node_graph_manager_test(Config) ->
     [{12, false, [], [], []}] = node_graph_manager:get_graph_updates(11),
     [{11, false, [], [], []}, {12, false, [], [], []}] = node_graph_manager:get_graph_updates(10),
-    [{11, false, [], [], []}, {12, false, [], [], []}] = node_graph_manager:get_graph_updates(-100).
+    [{11, false, [], [], []}, {12, false, [], [], []}] = node_graph_manager:get_graph_updates(-100),
+    ok = node_graph_manager:get_graph_updates(12).
+
+rebuild_graph_test(Config) ->
+    ok = node_graph_manager:rebuild_graph().
