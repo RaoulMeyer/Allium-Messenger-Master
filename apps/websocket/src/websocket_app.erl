b1e693b86fbb2228bc4c2f9c599bc34b1d5f58ae
Rik Harink <rik@blisss.nl>
2016-04-26 21:45:20 +0200

    Working websockets

--- apps/websocket/src/websocket_app.erl
+++ apps/websocket/src/websocket_app.erl
@@ -1,18 +1,25 @@
--module(websocket_app).  
--behaviour(application).  
--export([start/2, stop/1]).  
-  
-start(_StartType, _StartArgs) ->  
-  Dispatch = cowboy_router:compile([  
-      {'_', [  
-        {"/", cowboy_static, {priv_file, websocket, "index.html"}},  
-        {"/websocket", websocket_handler, []},
-        {"/static/[...]", cowboy_static, {priv_dir, websocket, "static"}}
-      ]}  
-    ]),  
-    {ok, _} = cowboy:start_http(http, 100, [{port, 8080}],  
-        [{env, [{dispatch, Dispatch}]}]),  
-      websocket_sup:start_link().
+%% Feel free to use, reuse and abuse the code in this file.
 
-stop(_State) ->  
-    ok.
+%% @private
+-module(websocket_app).
+-behaviour(application).
+
+%% API.
+-export([start/2]).
+-export([stop/1]).
+
+%% API.
+start(_Type, _Args) ->
+  Dispatch = cowboy_router:compile([
+    {'_', [
+      {"/", cowboy_static, {priv_file, websocket, "index.html"}},
+      {"/websocket", ws_handler, []},
+      {"/static/[...]", cowboy_static, {priv_dir, websocket, "static"}}
+    ]}
+  ]),
+  {ok, _} = cowboy:start_http(http, 100, [{port, 8080}],
+    [{env, [{dispatch, Dispatch}]}]),
+  websocket_sup:start_link().
+
+stop(_State) ->
+  ok.
