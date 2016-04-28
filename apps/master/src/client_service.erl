9cdf6008cd0e9def07a627ba47219c06d7ca4833
Niels Opdam <Niels Opdam>
2016-04-28 20:22:16 +0200

    Removed unreachable paths

--- apps/master/src/client_service.erl
+++ apps/master/src/client_service.erl
@@ -7,12 +7,4 @@
 
 -spec client_register(list(), list()) -> any().
 client_register(Username, Password) ->
-    try auth_service:client_register(Username, Password) of
-        _ ->
-            ok
-    catch
-        error:usernametaken ->
-            error(usernametaken);
-        _:_ ->
-            error(somethingwentwrong)
-    end.
+    auth_service:client_register(Username, Password).
