60fd8a58c3f199d5c34ccae715efaf0bab55a169
Rik Harink <rik@blisss.nl>
2016-04-28 13:41:10 +0200

    Websocket pubsub implementation

--- apps/websocket/src/ws_handler.erl
+++ apps/websocket/src/ws_handler.erl
@@ -7,20 +7,24 @@
 -export([websocket_info/3]).
 -export([websocket_terminate/3]).
 
+subscribe(Event) ->
+    gproc:reg({p, l, {?MODULE, Event}}).
+
 init({tcp, http}, _Req, _Opts) ->
     {upgrade, protocol, cowboy_websocket}.
 
 websocket_init(_TransportName, Req, _Opts) ->
-    erlang:start_timer(1000, self(), <<"Hello!">>),
-    {ok, Req, undefined_state}.
+    subscribe(node_update),
+    {ok, Req, undefined_state, hibernate}.
 
 websocket_handle({text, Msg}, Req, State) ->
-    {reply, {text, << "That's what she said! ", Msg/binary >>}, Req, State};
+    {reply, {binary, Msg}, Req, State};
 websocket_handle(_Data, Req, State) ->
     {ok, Req, State}.
 
+websocket_info({?MODULE, node_update, Msg}, Req, State) ->
+    {reply, {binary, Msg}, Req, State, hibernate};
 websocket_info({timeout, _Ref, Msg}, Req, State) ->
-    erlang:start_timer(1000, self(), <<"How' you doin'?">>),
     {reply, {text, Msg}, Req, State};
 websocket_info(_Info, Req, State) ->
     {ok, Req, State}.
