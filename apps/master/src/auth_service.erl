7b9077448b17c0207f5f9b8f8330e5efd2ba52b8
Niels Opdam <Niels Opdam>
2016-05-02 10:12:45 +0200

    ASDKN-300 * Changed the register function because the check for an existing client with the same Username is now done inside persistence service * Added a logout function (partly other user story) because a client should be logged out when it is not sending a heartbeat

--- apps/master/src/auth_service.erl
+++ apps/master/src/auth_service.erl
@@ -3,22 +3,30 @@
 %% API
 -export([
     client_register/2,
-    client_verify/2
+    client_verify/2,
+    client_logout/1
 ]).
 
 -spec client_register(list(), list()) -> any().
-client_register(Username, Password) ->
-    try persistence_service:select_client(Username) of
-        undefined ->
-            persistence_service:insert_client(Username, undefined, undefined, Password);
-        _ ->
-            error(usernametaken)
-    catch
-        _:_ ->
-            error(somethingwentwrong)
-    end.
+client_register(Username, Password) when is_list(Username), is_list(Password) ->
+     persistence_service:insert_client(Username, undefined, undefined, Password).
 
 -spec client_verify(list(), list()) -> any().
 client_verify(Username, SecretHash) when is_list(Username), is_list(SecretHash) ->
-    {User, ActualSecretHash, PublicKey, Password} = persistence_service:select_client(Username),
-    SecretHash =:= ActualSecretHash.
+    try
+        {_, CorrectSecretHash, _, _} = persistence_service:select_client(Username),
+        SecretHash = CorrectSecretHash,
+        ok
+    catch
+        _:_ ->
+            error(clientnotverified)
+    end.
+
+-spec client_logout(list()) -> any().
+client_logout(Username) when is_list(Username) ->
+    try
+        persistence_service:update_client_hash(Username, undefined)
+    catch
+        _:_ ->
+            error(couldnotbeloggedout)
+    end.
