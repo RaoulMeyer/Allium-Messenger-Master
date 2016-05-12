cd1746080099a9e5368f9b2e314060f253d1ee1b
RaoulMeyer <raouljmeyer@gmail.com>
2016-05-12 16:11:23 +0200

    Changed function signatures to accept binary public keys

--- apps/master/test/auth_service_SUITE.erl
+++ apps/master/test/auth_service_SUITE.erl
@@ -44,7 +44,7 @@ client_verify_existing_user(_Config) ->
     Username = "Client1",
     SecretHash = "SECRETHASH123",
     meck:expect(persistence_service, select_client, fun(_Username) ->
-        {Username, SecretHash, "PUBLICKEY123", "Qwerty123"} end),
+        {Username, SecretHash, <<"PUBLICKEY123">>, "Qwerty123"} end),
 
     auth_service:client_verify(Username, SecretHash),
     test_helpers:check_function_called(persistence_service, select_client, [Username]).
@@ -53,7 +53,7 @@ client_verify_existing_user_wrong_hash(_Config) ->
     Username = "Client1",
     SecretHash = "SECRETHASH123",
     meck:expect(persistence_service, select_client, fun(_Username) ->
-        {Username, "OTHERSECRETHASH", "PUBLICKEY123", "Qwerty123"} end),
+        {Username, "OTHERSECRETHASH", <<"PUBLICKEY123">>, "Qwerty123"} end),
 
     test_helpers:assert_fail(fun auth_service:client_verify/2, [Username, SecretHash],
         error, clientnotverified, no_user_with_username),
