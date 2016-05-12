e9cef06eb4c735e54bb12747f0acc839af69e4a6
RaoulMeyer <raouljmeyer@gmail.com>
2016-05-12 14:39:32 +0200

    Removed publish event from init, move to subscribe event preventing all connections to receive the full graph

--- apps/websocket/src/ws_handler.erl
+++ apps/websocket/src/ws_handler.erl
@@ -9,7 +9,11 @@
 
 -spec subscribe(any()) -> any().
 subscribe(Event) ->
-    gproc:reg({p, l, {?MODULE, Event}}).
+    gproc:reg({p, l, {?MODULE, Event}}),
+    RandomEvent = atom_to_list(Event) ++ "_init",
+    gproc:reg({p, l, {?MODULE, RandomEvent}}),
+    gproc:send({p, l, {?MODULE, RandomEvent}}, {?MODULE, RandomEvent, get_full_graph()}),
+    gproc:unreg({p, l, {?MODULE, RandomEvent}}).
 
 -spec publish(atom(), any()) -> any().
 publish(Event, Data) ->
@@ -23,14 +27,6 @@ init({tcp, http}, _Req, _Opts) ->
 websocket_init(_TransportName, Req, _Opts) ->
     lager:info("New websocket connection intitialised."),
     subscribe(node_update),
-    Version = 0,
-    Graph = get_wrapped_message(
-        'GRAPHUPDATERESPONSE',
-        hrp_pb:encode(
-            {graphupdateresponse, node_graph_manager:get_graph_updates(Version)}
-        )
-    ),
-    publish(node_update, Graph),
     {ok, Req, undefined_state, hibernate}.
 
 -spec websocket_handle(tuple(), any(), any()) -> tuple().
@@ -43,17 +39,23 @@ websocket_handle(_Data, Req, State) ->
     {ok, Req, State}.
 
 -spec websocket_info(tuple(), any(), any()) -> tuple().
-websocket_info({?MODULE, node_update, Msg}, Req, State) ->
+websocket_info({?MODULE, _, Msg}, Req, State) ->
     lager:info("Received message from pub/sub sending it through the websocket."),
-    {reply, {binary, Msg}, Req, State, hibernate};
-websocket_info(Info, Req, State) ->
-    lager:info("Unknown message received ignoring. Info: ~p", [Info]),
-    {ok, Req, State}.
+    {reply, {binary, Msg}, Req, State, hibernate}.
 
 -spec websocket_terminate(any(), any(), any()) -> atom().
 websocket_terminate(_Reason, _Req, _State) ->
     ok.
 
 -spec get_wrapped_message(list(), list()) -> list().
-get_wrapped_message(Type, Msg) ->
+ get_wrapped_message(Type, Msg) ->
     hrp_pb:encode({encryptedwrapper, Type, Msg}).
+
+-spec get_full_graph() -> list().
+get_full_graph() ->
+    get_wrapped_message(
+        'GRAPHUPDATERESPONSE',
+        hrp_pb:encode(
+            {graphupdateresponse, node_graph_manager:get_graph_updates(0)}
+        )
+    ).
