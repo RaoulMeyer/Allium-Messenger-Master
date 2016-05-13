7be1d1c22c14b80e3d56362fe3820f36cf4b3f21
Bob Linthorst <bre.linthorst@student.han.nl>
2016-05-13 11:06:13 +0200

    Fout uit list comprehention gehaald.

--- apps/master/src/client_manager.erl
+++ apps/master/src/client_manager.erl
@@ -8,6 +8,6 @@
 %% Todo: Change select_all_clients to it's own function. for iteration 3.
 -spec return_all_clients_by_hash(integer()) -> list().
 return_all_clients_by_hash(Hash) when Hash > 0 ->
-  {Username, _, PublicKey, _, ConnectedNodes} = persistence_service:select_all_clients(),
-  {Username, PublicKey, ConnectedNodes}.
+  [{Username, PublicKey, ConnectedNodes} ||
+    {Username, _, PublicKey, _, ConnectedNodes} <- persistence_service:select_all_clients()].
 
